d407d7ee3a304caf298d56fd9946d58b6fd07aee
kaialsh705 <kailashsharmaansh1999@gmail.com>
2023-12-02 11:59:12 +0530

    stable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import ProjectsPage from './components/projects'
 import WhatIKnowPage from './components/whatIKnow'
 import SocialLink from './components/socialLink'
 import ContactPage from './components/contactForm'
-import ResumePage from './components/resume'
 export default function Home() {
   return (
     <main>
